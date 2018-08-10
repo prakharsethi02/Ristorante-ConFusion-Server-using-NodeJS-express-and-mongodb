a29080c7ef4b4123183d83a8c19b900b5c34321e
Prakhar Sethi <sethi.prakhar@gmail.com>
2018-08-09 21:47:36 -0700

    mongoose

---
+++ node-mongoose1/models/dishes.js
@@ -0,0 +1,42 @@
+const mongoose =  require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema ({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required:true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+
+    },
+    comment: [commentSchema]
+},
+{
+    usePushEach: true,
+    timestamps:true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
